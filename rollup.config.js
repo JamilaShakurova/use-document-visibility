e7ba3cef9bf1a7186f47af058b3cbddcd9526a1a
Jamila90 <jamila.shakurova@gmail.com>
2022-06-06 08:22:53 +0300

    настраивает rollup

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
+import {nodeResolve} from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
+import typescript from "rollup-plugin-typescript";
+
+export default {
+    input: "src/index.tsx",
+    output: {
+        file: "build/bundle.js",
+        format: "iife",
+        sourcemap: true,
+    },
+    plugins: [
+        nodeResolve({
+            extensions: [".ts", ".tsx"]
+        }),
+        typescript(),
+        replace({
+            preventAssignment: true,
+            'process.browser': true,
+            'process.env.NODE_ENV': JSON.stringify('development')
+        }),
+        commonjs(),
+        serve({
+            contentBase: ["", "public"],
+            host: "localhost",
+            port: 3000,
+        }),
+        livereload({watch: "src/**/*"}),
+    ]
+};
---
+++ src/components/App/App.tsx
@@ -0,0 +1,11 @@
+// import '../../scss/app.scss';
+import React from "react";
+
+const App = () => {
+  return (
+      <section className="wrapper">
+        kokokl
+      </section>);
+}
+
+export default App;
