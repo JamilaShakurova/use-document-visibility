6bc920ffcdb739b3781d48ba8d2c3e6b994e7b15
Jamila90 <jamila.shakurova@gmail.com>
2022-06-09 09:01:12 +0300

    вносит изменения в rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -9,7 +9,7 @@ export default {
     input: "src/index.tsx",
     output: {
         file: "dist/index.js",
-        format: "cjs",
+        format: "iife",
         sourcemap: true,
     },
     plugins: [
